874b026563f937b7d65a6dce6cdea35348097697
Valter Pereira Oliveira <95293263+ValterOliveiraX@users.noreply.github.com>
2024-08-22 16:03:43 -0300

    adicionado ler arquivo

--- src/Questao01/Aresta.java
+++ src/Questao01/Aresta.java
@@ -9,6 +9,11 @@ public class Aresta {
     public Aresta(Vertice origem, Vertice destino) {
         this.origem = origem;
         this.destino = destino;
+    }
+
+    public Aresta(Vertice origem, Vertice destino, double peso) {
+        this.origem = origem;
+        this.destino = destino;
         this.peso = peso;
     }
 
